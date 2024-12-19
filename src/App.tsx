01ed7971c8a4d063750c06a0340353d557d2b2e3
Denis Biryukov <denis-biryukov-13@mail.ru>
2024-12-19 22:05:19 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [message, setMessage] = useState([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'},
+    ]);
+
+    return (
+        <div className="App">
+            <input/>
+            <button>+</button>
+            {message.map( (elem, index) => {
+                return (
+                    <div key={index}>{elem.message}</div>
+                )
+            } )}
+        </div>
+    );
 }
 
 export default App;
