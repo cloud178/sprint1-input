b7ba719ecce965bf3932ebe80543dfc7bcdad02f
Denis Biryukov <denis-biryukov-13@mail.ru>
2024-12-19 23:41:21 +0300

    sharing input + button

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,18 @@ function App() {
         {message: 'message3'},
     ]);
 
+    const addMessage = (newMessage: string) => {
+        setMessage([{message: newMessage}, ...message]);
+    }
+
     return (
         <div className="App">
-            <FullInput/>
-            {message.map( (elem, index) => {
+            <FullInput addMessage={addMessage}/>
+            {message.map((elem, index) => {
                 return (
                     <div key={index}>{elem.message}</div>
                 )
-            } )}
+            })}
         </div>
     );
 }
--- src/components/FullInput.tsx
+++ src/components/FullInput.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
-export const FullInput = () => {
+type FullInputPropsType = {
+    addMessage: (newMessage: string) => void
+}
+
+export const FullInput = ({addMessage}: FullInputPropsType) => {
+    let [title, setTitle] = useState<string>('');
+
+    const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value);
+    }
+
+    const onClickButtonHandler = () => {
+        addMessage(title);
+        setTitle('');
+    }
+
     return (
         <div>
-            <input/>
-            <button>+</button>
+            <input onChange={onChangeInputHandler} value={title}/>
+            <button onClick={onClickButtonHandler}>+</button>
         </div>
     );
 };
